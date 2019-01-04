a8aa283bdbefb410470a736658c2e810b6402e50
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-01-03 20:39:21 -0800

    setup complete

--- proxy/template/template.js
+++ proxy/template/template.js
@@ -3,20 +3,24 @@ module.exports = restaurantId => (
     <html lang="en">
       <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="/style.css">
+        <link rel="stylesheet" href="/tyler-style.css">
         <title>Lorem Yelpsum</title>
       </head>
       <body>
-        <div id="header"></div>
-        <div id="gallery"></div>
-        <div id="reservations"></div>
-        <div id="reviews"></div>
+        <div class="top">
+          <div id="header"></div>
+          <div id="gallery"></div>
+        </div>
+        <div class="bottom">
+            <div id="sidebar"></div>
+            <div id="reviews"></div>
+        </div>
         <script src="/lib/react.development.js"></script>
         <script src="/lib/react-dom.development.js"></script>
-        <script src="http://localhost:3001/bundle.js"></script>
-        <script src="http://localhost:5002/bundle.js"></script>
-        <script src="http://localhost:3003/bundle.js"></script>
-        <script src="http://localhost:3005/bundle.js"></script>
+        <script src="http://127.0.0.1:3001/bundle.js"></script>
+        <script src="http://127.0.0.1:5002/bundle.js"></script>
+        <script src="http://127.0.0.1:3003/bundle.js"></script>
+        <script src="http://127.0.0.1:3005/bundle.js"></script>
         <script>
           ReactDOM.render(
             React.createElement(Header, {restaurantId: ${restaurantId}}, null),
@@ -27,8 +31,8 @@ module.exports = restaurantId => (
             document.getElementById('gallery')
           );
           ReactDOM.render(
-            React.createElement(Reservations, {restaurantId: ${restaurantId}}, null),
-            document.getElementById('reservations')
+            React.createElement(Sidebar, {restaurantId: ${restaurantId}}, null),
+            document.getElementById('sidebar')
           );
           ReactDOM.render(
             React.createElement(Reviews, {restaurantId: ${restaurantId}}, null),
